8af1a958014976fefd4f42bf752fa2eac5873ca8
Arshit nag <arshitnag09@gmail.com>
2026-02-23 09:26:16 +0530

    Add files via upload

---
+++ OOPSBanners.java
@@ -0,0 +1,13 @@
+public class OOPSBanners {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", "********************"));
+        System.out.println(String.join("", "*   OOO   OOO   SSS *"));
+        System.out.println(String.join("", "*  O   O O   O S    *"));
+        System.out.println(String.join("", "*  O   O O   O  SSS  *"));
+        System.out.println(String.join("", "*  O   O O   O     S *"));
+        System.out.println(String.join("", "*   OOO   OOO   SSS  *"));
+        System.out.println(String.join("", "********************"));
+    }
+    
+}
